eb0fb27674a778bbd9d8a275c97054ca6e240110
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-05 18:51:26 -0300

    [ADD] init projet with porsche desing system

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { PorscheDesignSystemModule } from '@porsche-design-system/components-angular';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PorscheDesignSystemModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
