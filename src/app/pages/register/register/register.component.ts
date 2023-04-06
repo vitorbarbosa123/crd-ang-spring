71d6e5600e85a7404fa9dea8b8da37ebe5134132
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-06 12:28:11 -0300

    [ADD] register page

--- src/app/pages/register/register/register.component.ts
+++ src/app/pages/register/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import type { SwitchChangeEvent } from '@porsche-design-system/components-angular';
+import type { AccordionChangeEvent } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   clicked:boolean = false;
+  checked: boolean = true;
+  personalData = true;
+  location = true;
+  work = true;
+  registerType: string = 'Fornecedor';
+  name: string = 'Nome Completo:';
+  cnpjOrCpf: string = 'CPF:';
 
   constructor() { }
 
@@ -17,4 +26,29 @@ export class RegisterComponent implements OnInit {
   openSidebar() {
     this.clicked = !this.clicked;
   }
+
+  onAccordion1Change(e: CustomEvent<AccordionChangeEvent>) {
+    this.personalData = e.detail.open;
+  }
+
+  onAccordion2Change(e: CustomEvent<AccordionChangeEvent>) {
+    this.location = e.detail.open;
+  }
+  onAccordion3Change(e: CustomEvent<AccordionChangeEvent>) {
+    this.work = e.detail.open;
+  }
+
+  onSwitchChange(e: CustomEvent<SwitchChangeEvent>) {
+    this.checked = e.detail.checked;
+    if(this.checked) {
+      this.registerType = 'Fornecedor';
+      this.name = 'Nome Completo:';
+      this.cnpjOrCpf = 'CPF:';
+    } else {
+      this.registerType = 'Empresa';
+      this.name = 'Nome Fantasia:';
+      this.cnpjOrCpf = 'CNPJ:';
+    }
+  }
+
 }
