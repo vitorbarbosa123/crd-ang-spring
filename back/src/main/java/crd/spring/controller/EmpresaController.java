144237c4fcb9c04956dc83dd60b3d57537ee8d15
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-09 17:49:20 -0300

    [ADD] empresa repository

--- back/src/main/java/crd/spring/controller/EmpresaController.java
+++ back/src/main/java/crd/spring/controller/EmpresaController.java
@@ -1,18 +1,23 @@
 package crd.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import crd.spring.empresa.Empresa;
+import crd.spring.empresa.EmpresaRepository;
 
 @RestController
 @RequestMapping("empresa")
 public class EmpresaController {
+
+    @Autowired
+    private EmpresaRepository repository;
     
     @PostMapping
     public void cadastrar(@RequestBody Empresa empresa) {
-        System.out.println(empresa);
+        repository.save(new Empresa(empresa));
     }
 }
---
+++ back/src/main/java/crd/spring/empresa/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package crd.spring.empresa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+    
+}
