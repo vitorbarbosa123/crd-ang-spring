a25e2c0599533e8f89727e62a23fee1768bc634c
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-08 22:44:35 -0300

    [ADD] spring project

---
+++ back/src/main/java/crd/spring/CrdSpringAngApplication.java
@@ -0,0 +1,13 @@
+package crd.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrdSpringAngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrdSpringAngApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/crd/spring/CrdSpringAngApplicationTests.java
@@ -0,0 +1,13 @@
+package crd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrdSpringAngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
