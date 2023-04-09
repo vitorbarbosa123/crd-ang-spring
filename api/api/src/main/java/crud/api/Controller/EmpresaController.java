41cc518e596a9d4f7940c3fc41c40357c6cadcf6
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-09 20:37:38 -0300

    [ADD] pagination + sql logs

--- api/api/src/main/java/crud/api/Controller/EmpresaController.java
+++ api/api/src/main/java/crud/api/Controller/EmpresaController.java
@@ -3,6 +3,9 @@ package crud.api.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,11 +33,9 @@ public class EmpresaController {
     }
 
     @GetMapping
-    public List<DadosListagemEmpresa> listarEmpresa() {
+    public Page<DadosListagemEmpresa> listarEmpresa(@PageableDefault(size=10, sort={"nome"}) Pageable paginacao) {
         return repository
-                .findAll()
-                .stream()
-                .map(DadosListagemEmpresa::new)
-                .toList();
+                .findAll(paginacao)
+                .map(DadosListagemEmpresa::new);
     }
 }
