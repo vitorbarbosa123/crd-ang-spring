9e197b1365b393092fe277761edf78182c4a8123
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-08 22:15:14 -0300

    [ADD] list component

--- src/app/pages/list/list/list.component.ts
+++ src/app/pages/list/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import type { SwitchChangeEvent } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'app-list',
@@ -8,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
 
   clicked:boolean = false;
+  checked: boolean = true;
+  registerType: string = 'Fornecedor';
+  name: string = 'Nome Completo:';
+  cnpjOrCpf: string = 'CPF:';
 
   constructor() { }
 
@@ -16,5 +21,18 @@ export class ListComponent implements OnInit {
 
   openSidebar() {
     this.clicked = !this.clicked;
+  }
+
+  onSwitchChange(e: CustomEvent<SwitchChangeEvent>) {
+    this.checked = e.detail.checked;
+    if(this.checked) {
+      this.registerType = 'Fornecedor';
+      this.name = 'Nome Completo:';
+      this.cnpjOrCpf = 'CPF:';
+    } else {
+      this.registerType = 'Empresa';
+      this.name = 'Nome Fantasia:';
+      this.cnpjOrCpf = 'CNPJ:';
+    }
   }
 }
