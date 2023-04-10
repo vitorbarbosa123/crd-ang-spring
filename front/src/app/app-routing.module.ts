28c979513473bf320fa8973405b0c1586c4e1727
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-10 15:37:15 -0300

    [ADD] guards to routes

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginGuard } from './core/guards/login/login.guard';
+import { AutenticateGuard } from './core/guards/autenticate/autenticate.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: "full", component: HomeComponent}
+  {
+    path: '',
+    pathMatch: "full",
+    redirectTo: "login"
+  },
+  { 
+    path: '', 
+    redirectTo: "/home",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    canLoad: [LoginGuard]
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    canLoad: [AutenticateGuard]
+  },
 ];
 
 @NgModule({
