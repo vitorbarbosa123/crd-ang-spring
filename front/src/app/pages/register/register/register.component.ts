e32c6cd670e2e5e895566d06ccf388fc47d7c121
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-10 18:40:16 -0300

    [ADD] register component: styles, mask 4 inputs + logic

--- front/src/app/pages/register/register/register.component.ts
+++ front/src/app/pages/register/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import type { SwitchChangeEvent } from '@porsche-design-system/components-angular';
 import type { AccordionChangeEvent } from '@porsche-design-system/components-angular';
+import { mask, unMask } from 'remask';
 
 @Component({
   selector: 'app-register',
@@ -11,12 +12,11 @@ export class RegisterComponent implements OnInit {
 
   clicked:boolean = false;
   checked: boolean = true;
-  personalData = true;
-  location = true;
-  work = true;
-  registerType: string = 'Fornecedor';
-  name: string = 'Nome Completo:';
-  cnpjOrCpf: string = 'CPF:';
+  personalData: boolean = true;
+  location: boolean = true;
+  work: boolean = true;
+  inputValue: any;
+  isCpf: boolean = false;
 
   constructor() { }
 
@@ -40,15 +40,15 @@ export class RegisterComponent implements OnInit {
 
   onSwitchChange(e: CustomEvent<SwitchChangeEvent>) {
     this.checked = e.detail.checked;
-    if(this.checked) {
-      this.registerType = 'Fornecedor';
-      this.name = 'Nome Completo:';
-      this.cnpjOrCpf = 'CPF:';
-    } else {
-      this.registerType = 'Empresa';
-      this.name = 'Nome Fantasia:';
-      this.cnpjOrCpf = 'CNPJ:';
-    }
+    
   }
 
+  setMask() {
+    this.inputValue = mask(unMask(this.inputValue), ['999.999.999-99', '99.999.999/9999-99'])
+    if(this.inputValue.length == 14 && this.checked) {
+      this.isCpf = true;
+    } else {
+      this.isCpf = false;
+    }
+  }
 }
