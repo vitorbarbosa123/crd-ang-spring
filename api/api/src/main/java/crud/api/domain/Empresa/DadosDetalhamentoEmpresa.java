7badae7bd638e597b9f45390a53f444a234938e6
vitor barbosa <vitorbarbosa789@gmail.com>
2023-04-11 01:39:54 -0300

    [RFC] params order

--- api/api/src/main/java/crud/api/domain/Empresa/DadosDetalhamentoEmpresa.java
+++ api/api/src/main/java/crud/api/domain/Empresa/DadosDetalhamentoEmpresa.java
@@ -3,10 +3,10 @@ package crud.api.domain.Empresa;
 import crud.api.domain.Endereco.Endereco;
 import jakarta.validation.Valid;
 
-public record DadosDetalhamentoEmpresa(Long id, String nome, String cnpj, Endereco endereco) {
+public record DadosDetalhamentoEmpresa(Long id, String cnpj, Endereco endereco, String nome) {
 
     public DadosDetalhamentoEmpresa(@Valid Empresa empresa) {
-        this(empresa.getId(), empresa.getCnpj(), empresa.getNome(), empresa.getEndereco());
+        this(empresa.getId(), empresa.getCnpj(), empresa.getEndereco(), empresa.getNome());
     }
 
 }
